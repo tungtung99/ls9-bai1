b8bc7108882d10f302344b99f2631c698ad9f748
tungtung99 <tungnguyen99hn1@gmail.com>
2020-10-11 19:31:28 +0700

    hoanthanh

---
+++ index.js
@@ -0,0 +1,11 @@
+function input(){
+    a=prompt("Nhap so nguyen a: ");
+    b=prompt("Nhap so nguyen b: ");
+}
+function xuly(){
+    a1=parseInt(a);
+    b1=parseInt(b);
+}
+function output(){
+    document.write("Tong hai số nguyên a và b: "+((a1)+(b1)))
+} 
